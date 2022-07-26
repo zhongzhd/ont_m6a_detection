bd7afcd5cb6a7b0260df318d89c96953bec82eb5
zhongzhd <71424789+zhongzhd@users.noreply.github.com>
2022-07-26 10:26:18 +0800

    Create Nanocompore.sh

---
+++ m6A_detection/Nanocompore.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+
+ref=path_to_reference_transcriptome
+bed12=path_to_transcriptome_annotation
+wt_fastq=path_to_fastq_file
+ko_fastq=path_to_fastq_file
+wt_bam=path_to_bam_file
+ko_bam=path_to_bam_file
+
+#bed12 format
+#chr start end transcript * strand thickstart thickend * * blocksizes blockstarts
+#chr1	3073252	3074322	ENSMUST00000193812.1	0	+	3074322	3074322	0	1	1070,	0,
+
+#eventalign with samples
+nanopolish eventalign --reads $wt_fastq \
+--bam $wt_bam \
+--genome $ref \
+--samples \
+--print-read-names \
+--scale-events \
+--threads 16 > wt_eventalign.txt
+
+nanopolish eventalign --reads $ko_fastq \
+--bam $ko_bam \
+--genome $ref \
+--samples \
+--print-read-names \
+--scale-events \
+--threads 16 > ko_eventalign.txt
+
+#eventalign_collapse
+NanopolishComp Eventalign_collapse -i wt_eventalign.txt \
+-o wt \
+-t 16
+
+NanopolishComp Eventalign_collapse -i ko_eventalign.txt \
+-o ko \
+-t 16
+
+#samples comparison for detecting modifications
+nanocompore sampcomp --file_list1 wt/out_eventalign_collapse.tsv \
+--file_list2 ko/out_eventalign_collapse.tsv \
+--label1 wt --label2 ko \
+--fasta $ref \
+--bed $bed12 \
+--outpath nanocompore \
+--min_coverage 5 \
+--min_ref_length 10 \
+--allow_warnings \
+--nthreads 16
