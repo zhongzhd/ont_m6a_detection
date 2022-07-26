a4ff8be027306b5c1ce1592e5a35be45f5ae7c78
zhongzhd <71424789+zhongzhd@users.noreply.github.com>
2022-07-26 10:44:27 +0800

    Create Xpore.sh

---
+++ m6A_detection/Xpore.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+m6anet=path_to_m6Anet_results
+
+#data preparation
+xpore dataprep --eventalign $m6anet/wt_eventalign.txt \
+--out_dir wt \
+--n_processes 16 \
+--readcount_max 2000000
+
+xpore dataprep --eventalign $m6anet/ko_eventalign.txt \
+--out_dir ko \
+--n_processes 16 \
+--readcount_max 2000000
+
+#detect differential RNA modification
+xpore diffmod --config 1.yml \
+--n_processes 16
+
+#1.yml
+#notes: Pairwise comparison without replicates with default parameter setting.
+#data:
+#    ko:
+#        rep1: ko
+#    wt:
+#        rep1: wt
+#out: diffmod
+#criteria:
+#    readcount_min: 5
+#    readcount_max: 2000000
