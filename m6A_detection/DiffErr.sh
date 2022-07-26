e035e81cc4a2b2ac30f4a89c7cf206268b59a610
zhongzhd <71424789+zhongzhd@users.noreply.github.com>
2022-07-26 10:53:14 +0800

    Create DiffErr.sh

---
+++ m6A_detection/DiffErr.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+ref=path_to_reference_transcriptome
+wt_bam=path_to_bam_file
+ko_bam=path_to_bam_file
+
+differr -a $ko_bam \
+-b $wt_bam \
+-r $ref \https://github.com/zhongzhd/ont_m6a_detection
+-o mesdifferr.bed \
+-f 2 \
+--median-expr-threshold 0 \
+--min-expr-threshold 0 \
+-p 16
