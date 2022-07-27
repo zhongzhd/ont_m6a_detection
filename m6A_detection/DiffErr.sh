bcf608dec77a417b2703594958273d34b55eb685
zhongzhd <71424789+zhongzhd@users.noreply.github.com>
2022-07-27 15:50:48 +0800

    Update DiffErr.sh

--- m6A_detection/DiffErr.sh
+++ m6A_detection/DiffErr.sh
@@ -7,7 +7,7 @@ ko_bam=path_to_bam_file
 differr -a $ko_bam \
 -b $wt_bam \
 -r $ref \https://github.com/zhongzhd/ont_m6a_detection
--o mesdifferr.bed \
+-o differr.bed \
 -f 2 \
 --median-expr-threshold 0 \
 --min-expr-threshold 0 \
